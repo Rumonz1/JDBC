8764f46d78ec03934c58ed30bc1ce37860f8f87b
Rumonz <lone_assassin@mail.ru>
2023-06-19 16:33:02 +0300

    1 задание готово

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,28 +1,28 @@
 import java.sql.*;
 public class Application {
     public static void main(String[] args) throws SQLException{
-//        final String user = "postgres";
-//        final String password = "predator777";
-//        final String url = "jdbc:postgresql://localhost:5432/skypro";
-//
-//        try (final Connection connection = DriverManager.getConnection(url, user, password);
-//             PreparedStatement statement = connection.prepareStatement("SELECT * FROM employee WHERE id = (?)")) {
-//            statement.setInt(1, 1);
-//            final ResultSet resultSet = statement.executeQuery();
-//
-//            while (resultSet.next()) {
-//                String firstNameOfEmployee = "Имя: "+ resultSet.getString("first_name");
-//                String lastNameOfEmployee = "Фамилия: "+ resultSet.getString("last_name");
-//                String genderOfEmployee = "Пол: "+ resultSet.getString("gender");
-//                int ageOfEmployee = resultSet.getInt("age");
-////                String cityOfEmployee = "Город: "+ resultSet.getString("city_name");
-//
-//
-//                System.out.println(firstNameOfEmployee);
-//                System.out.println(lastNameOfEmployee);
-//                System.out.println(genderOfEmployee);
-//                System.out.println(ageOfEmployee);
-//                System.out.println(cityOfEmployee);
+        final String user = "postgres";
+        final String password = "predator777";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+        try (final Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM employee left join city on employee.city_id = city.city_id WHERE id = (?) ")) {
+            statement.setInt(1, 1);
+            final ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String firstNameOfEmployee = "Имя: "+ resultSet.getString("first_name");
+                String lastNameOfEmployee = "Фамилия: "+ resultSet.getString("last_name");
+                String genderOfEmployee = "Пол: "+ resultSet.getString("gender");
+                int ageOfEmployee = resultSet.getInt("age");
+                String cityOfEmployee = "Город: "+ resultSet.getString("city_name");
+
+
+                System.out.println(firstNameOfEmployee);
+                System.out.println(lastNameOfEmployee);
+                System.out.println(genderOfEmployee);
+                System.out.println(ageOfEmployee);
+                System.out.println(cityOfEmployee);
             }
 
 
